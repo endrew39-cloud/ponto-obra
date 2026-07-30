644a2d1b2ad497b0d1cfec43718ee00c79b5699c
endrew39-cloud <endrew39@gmail.com>
2026-07-30 02:39:46 -0300

    Update RegistroPonto.kt

--- app/src/main/java/com/ponto/obra/RegistroPonto.kt
+++ app/src/main/java/com/ponto/obra/RegistroPonto.kt
@@ -1,34 +1,38 @@
 package com.ponto.obra
 
-import java.text.SimpleDateFormat
 import java.util.Date
-import java.util.Locale
 
 data class RegistroPonto(
-    val id: String = "",
-    val cpfFuncionario: String = "",
-    val tipo: String = "",
-    val dataHora: String = "",
-    val latitude: Double = 0.0,
-    val longitude: Double = 0.0,
-    val nomeObra: String = "",
-    val sincronizado: Boolean = false,
-    val hashSeguranca: String = ""
+    val cpf: String,
+    val tipo: String,
+    val lat: Double,
+    val lon: Double,
+    val nomeObra: String,
+    val dataHora: Date,
+    val enviado: Boolean = false
 ) {
+    fun paraMapa(): Map<String, Any?> {
+        return mapOf(
+            "cpf" to cpf,
+            "tipo" to tipo,
+            "latitude" to lat,
+            "longitude" to lon,
+            "nome_obra" to nomeObra,
+            "data_hora" to dataHora.time,
+            "enviado" to enviado
+        )
+    }
+
     companion object {
         fun criarNovo(cpf: String, tipo: String, lat: Double, lon: Double, obra: String): RegistroPonto {
-            val dataFormatada = SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale("pt", "BR")).format(Date())
-            val dadosBrutos = "$cpf|$tipo|$dataFormatada|$lat|$lon|$obra"
-            val hash = dadosBrutos.hashCode().toString()
-
             return RegistroPonto(
-                cpfFuncionario = cpf,
+                cpf = cpf,
                 tipo = tipo,
-                dataHora = dataFormatada,
-                latitude = lat,
-                longitude = lon,
+                lat = lat,
+                lon = lon,
                 nomeObra = obra,
-                hashSeguranca = hash
+                dataHora = Date(),
+                enviado = false
             )
         }
     }

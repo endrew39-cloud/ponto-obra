fc6436b653e2ff6283eda9eb02220b8ef4e87346
endrew39-cloud <endrew39@gmail.com>
2026-07-30 11:23:19 -0300

    Update RegistroPonto.kt

--- app/src/main/java/com/ponto/obra/RegistroPonto.kt
+++ app/src/main/java/com/ponto/obra/RegistroPonto.kt
@@ -1,39 +1,21 @@
-package com.ponto.obra
-
-import java.util.Date
-
 data class RegistroPonto(
     val cpf: String,
+    val nome: String,
+    val funcao: String,
     val tipo: String,
-    val lat: Double,
-    val lon: Double,
-    val nomeObra: String,
-    val dataHora: Date,
-    val enviado: Boolean = false
+    val latitude: Double,
+    val longitude: Double,
+    val nomeObra: String
 ) {
-    fun paraMapa(): Map<String, Any?> {
+    fun paraMapaJson(): Map<String, Any> {
         return mapOf(
             "cpf" to cpf,
+            "nome" to nome,
+            "funcao" to funcao,
             "tipo" to tipo,
-            "latitude" to lat,
-            "longitude" to lon,
-            "nome_obra" to nomeObra,
-            "data_hora" to dataHora.time,
-            "enviado" to enviado
+            "latitude" to latitude,
+            "longitude" to longitude,
+            "nomeObra" to nomeObra
         )
-    }
-
-    companion object {
-        fun criarNovo(cpf: String, tipo: String, lat: Double, lon: Double, obra: String): RegistroPonto {
-            return RegistroPonto(
-                cpf = cpf,
-                tipo = tipo,
-                lat = lat,
-                lon = lon,
-                nomeObra = obra,
-                dataHora = Date(),
-                enviado = false
-            )
-        }
     }
 }

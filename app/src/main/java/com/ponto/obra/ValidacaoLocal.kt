6383a6409101c884ca60ef52faf3b8aa8b3480c3
endrew39-cloud <endrew39@gmail.com>
2026-07-30 01:16:06 -0300

    Update ValidacaoLocal.kt

--- app/src/main/java/com/ponto/obra/ValidacaoLocal.kt
+++ app/src/main/java/com/ponto/obra/ValidacaoLocal.kt
@@ -2,16 +2,18 @@ package com.ponto.obra
 
 import kotlin.math.*
 
-data class ObraCadastrada(
-    val nome: String,
-    val latitude: Double,
-    val longitude: Double,
-    val raioPermitidoMetros: Double
-)
-
 object ValidacaoLocal {
-    fun calcularDistancia(lat1: Double, lon1: Double, lat2: Double, lon2: Double): Double {
-        val raioTerra = 6371000
+    fun estaDentroDaObra(latUsuario: Double, lonUsuario: Double, obra: Obra): Boolean {
+            // Coordenada de teste, depois altera para a sua obra
+            val latObra = -22.9068
+            val lonObra = -43.1729
+            
+            val distancia = calcularDistancia(latUsuario, lonUsuario, latObra, lonObra)
+            return distancia <= obra.raioPermitidoMetros
+        }
+
+    private fun calcularDistancia(lat1: Double, lon1: Double, lat2: Double, lon2: Double): Double {
+        val raioTerra = 6371000 // metros
         val dLat = Math.toRadians(lat2 - lat1)
         val dLon = Math.toRadians(lon2 - lon1)
         val a = sin(dLat / 2) * sin(dLat / 2) +
@@ -19,13 +21,5 @@ object ValidacaoLocal {
                 sin(dLon / 2) * sin(dLon / 2)
         val c = 2 * atan2(sqrt(a), sqrt(1 - a))
         return raioTerra * c
-    }
-
-    fun estaDentroDaObra(latUsuario: Double, lonUsuario: Double, obra: ObraCadastrada): Boolean {
-        val distancia = calcularDistancia(
-            latUsuario, lonUsuario,
-            obra.latitude, obra.longitude
-        )
-        return distancia <= obra.raioPermitidoMetros
     }
 }
